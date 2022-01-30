43774d96fcae94fe015fe4f3efcea0c79052f23f
sarabsamarah <sarab.samarah@bryanuniversity.edu>
2022-01-29 19:35:16 -0800

    Week 1 Class and Kata

--- week1/class.py
+++ week1/class.py
@@ -11,20 +11,20 @@ class Oils:
     def changeList(self):
         self.oilList = tuple(self.oilList)
 
-print('x')
+print('o')
 
-x = Oils()
+o = Oils()
 
-x.printOils()
+o.printOils()
 
-x.addOils('Copaiba')
-x.printOils()
-x.addOils(("Frankensence", "Sage", "Lavendar"))
-x.printOils()
-x.addOils({'oilName':'Orange','type':'sweet'})
-x.printOils()
-x.addOils(1)
-x.printOils()
+o.addOils('Copaiba')
+o.printOils()
+o.addOils(("Frankensence", "Sage", "Lavendar"))
+o.printOils()
+o.addOils({'oilName':'Orange','type':'sweet'})
+o.printOils()
+o.addOils(1)
+o.printOils()
 
-x.changeList()
-x.printOils()
+o.changeList()
+o.printOils()
