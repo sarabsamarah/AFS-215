bac218c692af1ffb0dec65e3f36ac92fa9e973eb
sarabsamarah <sarab.samarah@bryanuniversity.edu>
2022-02-25 03:18:48 -0800

    Week 6

---
+++ week6/tests/capitalize.test.js
@@ -0,0 +1,17 @@
+const capitalize = require('../capitalize');
+
+test('should capitalize lowercase string correctly', () => {
+  expect(capitalize("capitalize")).toBe("Capitalize");
+});
+
+test("should return '' for strings with length 0", () => {
+    expect(capitalize("")).toBe("");
+});
+
+test("should return the same string for uppercase letters", () => {
+    expect(capitalize("UPPERCASE")).toBe("UPPERCASE");
+});
+
+test("should return an error for non-string inputs", () => {
+    expect(capitalize(21)).toBe(21);
+});
