7e98ec7c40bab514c5b6f1dd2318c080f202049d
sarabsamarah <sarab.samarah@bryanuniversity.edu>
2022-03-26 16:48:05 -0700

    Week 1

---
+++ capstone/capstone_project/untitled.py
@@ -0,0 +1,46 @@
+print('sample output on terminal ')
+a = 5
+print('The value of variable is', a)
+num = input('Enter a number: ')
+print('The value of variable is', num)
+
+
+
+if 'foo' in ['foo', 'baz', 'qux']:
+    print('Expression was true')
+    print('Executing statement in suite')
+    print('...')
+    print('Done in if loop')
+else:
+ print('Done in else loop')
+
+ i = 1 
+ while i < 60:
+
+class Employee:
+    def 
+    def __init__(self, first=None, last=None, id=None, pay=0.00):
+        self.firstName= first
+        self.lastName= lastself
+        self.employeeId= id 
+        self.hourlyPay= pay
+
+    def getFirstName(self):
+        return self.firstName
+    def getLastName(self):
+        return self.lastName
+    def getId(self):
+        return self.employeeId
+    def getHourlyPay(self):
+        return self.hourlyPay
+    def setHourlyPay(self, pay):
+        self.hourlyPay
+
+
+
+Data1 = (7, False, "Apple", True, 7, 98.6)
+print{len(Data1)}
+Data2 = {"July", 4, 8, "Tango", 4.3, "Bingo"}
+Data2.pop()
+Data2.add("Alpha")
+print(Data2)
